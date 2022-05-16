a0da7367b664f18af3d4f8ea7ad0ea8621b68439
К Клин <kklinchenkov@inbox.ru>
2022-05-16 21:07:00 +0300

    Добавьте файлы проекта.

---
+++ testDelegate/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+
+public class SomethingHappend : Exception
+{
+    public SomethingHappend(string message)
+        : base(message)
+    {
+    }
+}
+public class Account
+{
+    public void Check (int i)
+    {
+        Console.WriteLine(i);
+        if (i * 2 == 4)
+        {
+            // ??? Invoke("success")
+            throw new SomethingHappend("success");
+        }
+    }
+}
+public class Test
+{
+    static async Task Main()
+    {
+        var account = new Account();
+        
+        var result = await Job(account);
+
+        Console.WriteLine(result);
+    }
+
+    static async Task<string> Job(Account account)
+    {
+        int i = 0;
+
+        while (true)
+        {
+            try
+            {
+                account.Check(i);
+                // if i*2 == 10 return "Success"
+                i++;
+                await Task.Delay(1000);
+            }
+            catch (SomethingHappend ex)
+            {
+                return ex.Message;
+            }
+
+        }
+    }
+}
